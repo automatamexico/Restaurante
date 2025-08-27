23c70c0d336a9d42d11eb80162a9a5625ad6f660
automatamexico <desarrollo.solucionesdelsu@gmail.com>
2025-08-26 22:03:42 -0600

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-      plugins: {
-        tailwindcss: {},
-      },
-    };
-
-    export default config
+// postcss.config.js  (CommonJS para CRA)
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
