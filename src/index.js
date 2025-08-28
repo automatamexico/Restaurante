10b78ba317e7b9b56cd62346eb81f269d06b1490
automatamexico <desarrollo.solucionesdelsu@gmail.com>
2025-08-27 20:06:38 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 import App from "./App";
+import ErrorBoundary from "./ErrorBoundary";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>
 );
