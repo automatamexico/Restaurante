92013d9791f63d17e541aea57ce701dc28e3b52e
automatamexico <desarrollo.solucionesdelsu@gmail.com>
2025-08-27 19:10:53 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-<!-- public/index.html -->
-<!DOCTYPE html>
-<html lang="es">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>DelSabor</title>
+// src/index.js
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+import App from "./App";
 
-    <!-- QZ Tray (impresión silenciosa) -->
-    <script src="https://cdn.jsdelivr.net/npm/qz-tray@2.2.7/qz-tray.js"></script>
-  </head>
-  <body>
-    <noscript>Necesitas habilitar JavaScript para ejecutar esta app.</noscript>
-    <div id="root"></div>
-  </body>
-</html>
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
